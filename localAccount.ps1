7f96b444c96b8fbfded49a02aec8f05bbeff0a2f
D3ENNY <denysraimondi06@gmail.com>
2024-04-18 17:03:43 +0200

    add script

---
+++ localAccount.ps1
@@ -0,0 +1,14 @@
+# $Password = Read-Host -AsSecureString "Insert account's password"
+# $Name = Read-Host "Insert account's name"
+# $Desc = Read-Host "Insert account's description"
+
+$Password = "Concorso2024!!!"
+$Name = "concorso"
+$Desc = "account concorso 2024"
+$params = @{
+    Name        = $Name
+    Password    = $Password
+    FullName    = $Name
+    Description = $Desc
+}
+New-LocalUser @params
---
+++ removeAccount.ps1
@@ -0,0 +1,3 @@
+# $Name = Read-Host "insert account's name"
+$Name = "concorso"
+Remove-LocalUser -Name $Name
